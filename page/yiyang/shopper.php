28445f040ef9349a78016e78b211c55349397ecc
Yiyang <otakublack62@gmail.com>
2020-04-28 11:52:51 +0800

    幫以楊除蟲 側欄修復

---
+++ page/yiyang/shopper.php
@@ -0,0 +1,10 @@
+<?php
+require_once('../../db.inc.php');
+require_once('../../checkSession.php');
+require_once("./shopheader.php");
+require_once('../../components/navmenu_left.php');
+require_once("./shopbody.php");
+require_once("./addshop.php");
+require_once("./shopfooter.php");
+?>
+
