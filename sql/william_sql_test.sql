c68fe2bfc58a41ec5e001816baa672bc4adeb07b
william_8026 <zhuang8026@gmail.com>
2020-07-21 22:18:25 +0800

    test

--- sql/william_sql_test.sql
+++
@@ -1,4 +0,0 @@
-SELECT `itemId`,`itemName`, `itemImg`, `colorid`,`itemsbrand`, `itemstype`, 
-`itemPrice`, `itemQty`, `itemsstar`, `itemCategoryId`, `itemscontent`, `created_at`, `updated_at`
-FROM `items` 
-ORDER BY `itemId` ASC
